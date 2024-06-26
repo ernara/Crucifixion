6104f490a33cc5f6e298e94dc0cf54051d95c70d
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2024-06-27 00:39:16 +0300

    Add files via upload

---
+++ FileReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Crucifixion
+{
+    internal class FileReader
+    {
+        public static List<string> ReadFile()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            string relativePath = @"..\..\..\upes.csv";
+            string fullPath = Path.GetFullPath(Path.Combine(currentDirectory, relativePath));
+
+            List<string> lines = new List<string>(File.ReadAllLines(fullPath));
+
+            List<string> filteredLines = new List<string>();
+
+            foreach (string line in lines)
+            {
+                int length = line.Length;
+                if (length < 4 || length == 5 || length == 8 || length > 9)
+                {
+                    
+                }
+                else
+                {
+                    filteredLines.Add(line);
+                }
+            }
+
+            return filteredLines;
+        }
+    }
+}
---
+++ WordChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crucifixion
+{
+    public static class WordChecker
+    {
+        public static List<int> wordLengths { get; } = new List<int>
+        {
+            9,4,7,6,4,9,4,9,6,9
+
+            //4 3
+            //6 2
+            //7 1
+            //9 4
+        };
+
+
+        public static bool CheckWord(this string word, int wordNumber)
+        {
+            if (word.Length == wordLengths[wordNumber - 1])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CheckWord(this string word, int wordNumber, int index, char letter)
+        {
+            if (word.Length == wordLengths[wordNumber - 1] && word[index]==letter)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
